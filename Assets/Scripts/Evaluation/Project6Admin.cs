3702a40e84694ac3d0c95bf69465df8f5b759c35
Isaiah Mann <contact@isaiahmann.com>
2016-10-29 02:03:32 -0400

    Adding basic structure for P6

---
+++ Assets/Scripts/Evaluation/Project6Admin.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Project6Admin : ProjectAdmin {
+	void Start () {
+		FactoryController.SetConveyorBeltSpeeds(5.0f);
+	}
+}
---
+++ Assets/Scripts/Projects/Project6.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Author(s): [Insert Your Name Here], Isaiah Mann
+ * Description: Project 6: Designing Objects
+ * Time to Complete: [Insert How Much Time It Took You to Complete the Assignment]
+ */
+
+public class Project6 : ProjectTemplate {
+
+	protected override void setupFactory () {
+		// NOTHING
+	}
+
+}
